c607fcf4c3d8dd5a2b8ddafddec34a1aa7ff2db0
danieldebrito <danieldebrito@outlook.com>
2019-03-30 19:28:03 -0300

    login

--- clasesApp/src/app/app.module.ts
+++ clasesApp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/layout/nav/nav.component';
+import { BodyComponent } from './components/layout/body/body.component';
+import { FootComponent } from './components/layout/foot/foot.component';
+import { LoginComponent } from './components/auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    BodyComponent,
+    FootComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
